cb36b0f5d6451c2f8b172b4c2f89c832cab682a7
Max Scheerer <scheerer@fzi.de>
2024-02-23 15:34:21 +0100

    Implement refinement by sampling for a partition.

--- rqdnn/manifold/partition_map.py
+++ rqdnn/manifold/partition_map.py
@@ -1,4 +1,6 @@
 from sklearn import tree
+from scipy.stats import qmc
+
 import numpy as np
 
 class ManifoldPartitionMap:
@@ -14,11 +16,10 @@ class ManifoldPartitionMap:
 
         print(tree.export_text(self.estimated_manifold))
 
-        decision_paths = self.estimated_manifold.decision_path(features)
         leave_nodes = self.estimated_manifold.apply(features)
-        n = len(features)
-        for i in range(n):
+        for i in range(len(features)):
             leave_node = leave_nodes[i]
+            feature = features[i,:]
 
             partitions = [p for p in self.partitioned_space if p.node_id == leave_node]
             if len(partitions) == 0:
@@ -29,21 +30,7 @@ class ManifoldPartitionMap:
             else:
                 partition = partitions[0]
 
-            nodes = decision_paths.indices[decision_paths.indptr[i] : decision_paths.indptr[i + 1]]
-            for node in nodes:
-                if leave_node == node:
-                    continue
-
-                feature = self.estimated_manifold.tree_.feature[node]
-                threshold = self.estimated_manifold.tree_.threshold[node]
-                if features[i, feature] <= threshold:
-                    max_val = threshold
-                    min_val = features[i, feature]
-                else:
-                    max_val = features[i, feature]
-                    min_val = threshold
-
-                partition.add_or_update_interval(feature, min_val, max_val)
+            partition.include(feature)
 
     def _prepare_for_analysis(self, rel_scores):
         n = len(rel_scores)
@@ -69,26 +56,71 @@ class ManifoldPartitionMap:
 class Partition:
     def __init__(self, node_id, rel_score) -> None:
         self.node_id = node_id
-        self.rel_score = rel_score 
-        self.feature_intervals = {}
+        self.rel_score = rel_score
+        self.feature_dim_ranges = {}
+        self.partitioned_features = []
 
     def __str__(self) -> str:
-        str_representation = "Partition with ID {id} and reliability score {score}: \n".format(id=self.node_id, score=self.rel_score)
-        for feature, interval in sorted(self.feature_intervals.items()):
-            str_representation += "{space}feature: {feature}, interval: [{min_val}, {max_val}]\n".format(space= "\t",
-                                                                                                         feature=feature, 
-                                                                                                         min_val=interval[0],
-                                                                                                         max_val=interval[1])
-        return str_representation
+        return "Partition with ID {id} and reliability score {score} and acc spans {acc} \n".format(id=self.node_id, 
+                                                                                                    score=self.rel_score, 
+                                                                                                    acc=self.accumulated_spans())
+    
+    def include(self, feature):
+        self.partitioned_features.append(feature)
 
-    def add_or_update_interval(self, feature, min_val, max_val):
-        if feature not in self.feature_intervals.keys():
-            self.feature_intervals[feature] = (min_val, max_val)
-        else:
-            current = self.feature_intervals[feature]
-            new_min_val = min_val if min_val < current[0] else current[0] 
-            new_max_val = max_val if max_val > current[1] else current[1]
+        for feature_dim in range(len(feature)):
+            feature_dim_val = feature[feature_dim]
+
+            if feature_dim not in self.feature_dim_ranges.keys():
+                min_val = max_val = feature_dim_val
+            else:
+                current_range = self.feature_dim_ranges[feature_dim]
+                min_val = feature_dim_val if feature_dim_val < current_range[0] else current_range[0] 
+                max_val = feature_dim_val if feature_dim_val > current_range[1] else current_range[1]
             
-            self.feature_intervals[feature] = (new_min_val, new_max_val)
+            self.feature_dim_ranges[feature_dim] = (min_val, max_val)
+
+    def accumulated_spans(self):
+        return sum(self._get_spans())
+    
+    def sample_features(self, num_samples=1000):
+        spans = np.array(self._get_spans())
+        non_zeros = spans != 0
+        non_zero_span_idxs = np.arange(len(spans))[non_zeros]
+        zero_span_idxs = np.arange(len(spans))[~non_zeros]
+        
+        is_singleton = len(spans) == len(zero_span_idxs)
+        if is_singleton:
+            return np.array([])
+
+        samples = qmc.Halton(d=len(non_zero_span_idxs), scramble=False).random(n=num_samples)
+        
+        non_zero_ranges = [feature_dim_range for feature_dim, feature_dim_range in self.feature_dim_ranges.items() 
+                              if feature_dim in non_zero_span_idxs]
+        l_bounds, u_bounds = zip(*non_zero_ranges)
+        scaled_samples = qmc.scale(samples, l_bounds, u_bounds)
+
+        num_part_features = len(self.partitioned_features)
+        num_rows = num_part_features + num_samples
+        num_columns = len(non_zero_span_idxs) + len(zero_span_idxs)
+        sampled_features = np.zeros((num_rows, num_columns))
+        sampled_features[range(num_part_features),:] = self.partitioned_features
+
+        singletons = [feature_dim_range[0] for feature_dim, feature_dim_range in self.feature_dim_ranges.items() 
+                       if feature_dim in zero_span_idxs]
+        for i in range(num_part_features, num_part_features + num_samples):
+            sample = scaled_samples[i - num_part_features,:]
+
+            enriched_sample = np.zeros(num_columns)
+            enriched_sample[zero_span_idxs] = singletons
+            enriched_sample[non_zero_span_idxs] = sample
+
+            sampled_features[i,:] = enriched_sample
+        
+        return sampled_features
+
+    def _get_spans(self):
+        return [range[1] - range[0] for _, range in self.feature_dim_ranges.items()]
+
 
 
