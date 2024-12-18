ecf9b4ee4e3f6537e244f4e00c0a193b4deb5640
Max Scheerer <scheerer@fzi.de>
2024-12-18 12:19:52 +0100

    Include model training in the main control flow. Exclude scin cancer evaluation because it requires installation instructions, not documented so far.

--- rqdnn/main.py
+++ rqdnn/main.py
@@ -6,15 +6,19 @@ from evaluation.report import EvaluationReport
 
 if __name__ == '__main__':
     eval = MNISTEvaluation()
+    eval.train_models()
     mnist_reports = eval.evaluate()
 
     eval = CIFAR10Evaluation()
+    eval.train_models()
     cifar_reports = eval.evaluate()
 
     eval = FashionMNISTEvaluation()
+    eval.train_models()
     fashion_report = eval.evaluate()
 
-    eval = SkinCancerEvaluation()
-    skin_report = eval.evaluate()
+    #eval = SkinCancerEvaluation()
+    #skin_report = eval.evaluate()
 
-    EvaluationReport.export(mnist_reports, cifar_reports, fashion_report, skin_report)
+    #EvaluationReport.export(mnist_reports, cifar_reports, fashion_report, skin_report)
+    EvaluationReport.export(mnist_reports, cifar_reports, fashion_report)
